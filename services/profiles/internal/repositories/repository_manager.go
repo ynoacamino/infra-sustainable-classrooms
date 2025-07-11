b2cf005ea9a209b26326224abd9063658c334f22
ynoacamino <ynoacamino@gmail.com>
2025-07-11 01:33:37 -0500

    feat(profiles-service): add auth service in repository manager

--- services/profiles/internal/repositories/repository_manager.go
+++ services/profiles/internal/repositories/repository_manager.go
@@ -3,20 +3,23 @@ package repositories
 import (
 	"github.com/jackc/pgx/v5/pgxpool"
 	"github.com/ynoacamino/infra-sustainable-classrooms/services/profiles/internal/ports"
+	"google.golang.org/grpc"
 )
 
 type RepositoryManager struct {
 	ProfileRepo        ports.ProfileRepository
 	StudentProfileRepo ports.StudentProfileRepository
 	TeacherProfileRepo ports.TeacherProfileRepository
+	AuthServiceRepo    ports.AuthServiceRepository
 	pool               *pgxpool.Pool
 }
 
-func NewRepositoryManager(pool *pgxpool.Pool) *RepositoryManager {
+func NewRepositoryManager(pool *pgxpool.Pool, grpccoon *grpc.ClientConn) *RepositoryManager {
 	return &RepositoryManager{
 		ProfileRepo:        NewProfileRepository(pool),
 		StudentProfileRepo: NewStudentProfileRepository(pool),
 		TeacherProfileRepo: NewTeacherProfileRepository(pool),
+		AuthServiceRepo:    NewAuthServiceRepository(grpccoon),
 		pool:               pool,
 	}
 }
