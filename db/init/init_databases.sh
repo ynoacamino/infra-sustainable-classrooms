3e781ed2364b9757fc291bdfa2f391d6e425702c
Yenaro Noa Camino <ynoacamino@gmail.com>
2025-07-12 09:58:33 -0500

    ci(text-service): change ci/cd files

--- db/init/init_databases.sh
+++ db/init/init_databases.sh
@@ -3,6 +3,7 @@ echo "Running SQL scripts"
 echo "🛠️ Create databases"
 psql -v ON_ERROR_STOP=1 --username "$POSTGRES_USER" --dbname "infrastructure_db" -c "CREATE DATABASE auth_db;"
 psql -v ON_ERROR_STOP=1 --username "$POSTGRES_USER" --dbname "infrastructure_db" -c "CREATE DATABASE profiles_db;"
+psql -v ON_ERROR_STOP=1 --username "$POSTGRES_USER" --dbname "infrastructure_db" -c "CREATE DATABASE text_db;"
 # psql -v ON_ERROR_STOP=1 --username "$POSTGRES_USER" -c "CREATE DATABASE users_db;"
 # psql -v ON_ERROR_STOP=1 --username "$POSTGRES_USER" -c "CREATE DATABASE payments_db;"
 # psql -v ON_ERROR_STOP=1 --username "$POSTGRES_USER" -c "CREATE DATABASE notifications_db;"
@@ -11,6 +12,7 @@ psql -v ON_ERROR_STOP=1 --username "$POSTGRES_USER" --dbname "infrastructure_db"
 echo "🛠️ Init databases"
 psql -v ON_ERROR_STOP=1 --username "$POSTGRES_USER" --dbname=auth_db < /docker-entrypoint-initdb.d/auth_db.sql
 psql -v ON_ERROR_STOP=1 --username "$POSTGRES_USER" --dbname=profiles_db < /docker-entrypoint-initdb.d/profiles_db.sql
+psql -v ON_ERROR_STOP=1 --username "$POSTGRES_USER" --dbname=text_db < /docker-entrypoint-initdb.d/text_db.sql
 # psql -v ON_ERROR_STOP=1 --username "$POSTGRES_USER" --dbname=users_db < /docker-entrypoint-initdb.d/users_db.sql
 # psql -v ON_ERROR_STOP=1 --username "$POSTGRES_USER" --dbname=payments_db < /docker-entrypoint-initdb.d/payments_db.sql
 # psql -v ON_ERROR_STOP=1 --username "$POSTGRES_USER" --dbname=notifications_db < /docker-entrypoint-initdb.d/notifications_db.sql
